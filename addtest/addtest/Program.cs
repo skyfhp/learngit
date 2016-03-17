17947ced1accf9c5326d42c2bc0aa2c2298449c9
skyfhp <fenghuapingfhp@126.com>
2016-03-17 15:39:59 +0800

    add Test

---
+++ addtest/addtest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace addtest
+{
+    class Program
+    {
+        public int Add(int a, int b)
+        { 
+           return a+b;
+        }
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
